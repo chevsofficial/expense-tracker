a8082d937058be3353c2a309459bc4f725e7e30a
Chevs <56131598+chevsofficial@users.noreply.github.com>
2026-01-18 11:41:07 -0500

    chore: initial scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="p-10">
+    <main className="p-10 space-y-4">
       <h1 className="text-3xl font-bold">Expense Tracker</h1>
-      <p className="mt-2">If this text looks styled, Tailwind is working.</p>
+      <button className="btn btn-primary">DaisyUI Button</button>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,4 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 } satisfies Config;
