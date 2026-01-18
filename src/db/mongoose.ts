061b9e0fe4e2042da7a0a2c2aa01c31dce5d97ec
Chevs <56131598+chevsofficial@users.noreply.github.com>
2026-01-18 18:59:42 -0500

    Update mongoose.ts

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Missing MONGODB_URI in environment variables");
-}
-
 /**
  * Prevent multiple connections during Next.js hot reload (dev)
  */
@@ -18,11 +12,17 @@ declare global {
 global._mongoose ||= { conn: null, promise: null };
 
 export async function dbConnect() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("Missing MONGODB_URI in environment variables");
+  }
+
   if (global._mongoose?.conn) return global._mongoose.conn;
 
   if (!global._mongoose?.promise) {
     global._mongoose!.promise = mongoose
-      .connect(MONGODB_URI!, { dbName: "expense-tracker" })
+      .connect(MONGODB_URI, { dbName: "expense-tracker" })
       .then((m) => m);
   }
 
