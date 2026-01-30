4d2021afb5d777caf35a5a7d4cf75126d0f26c88
Chevs <56131598+chevsofficial@users.noreply.github.com>
2026-01-29 23:17:23 -0600

    Fix daisyUI v5 setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="spendary">
+    <html lang="en" data-theme="spendary" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
